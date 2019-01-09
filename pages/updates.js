80211044b9d5528757df95c08672d346f3807496
Torben Werner <torbensky@users.noreply.github.com>
2019-01-08 19:13:44 -0800

    Added a project update note

--- pages/updates.js
+++ pages/updates.js
@@ -6,15 +6,30 @@ export default props => {
       <section className="section">
         <div className="container">
           <h1 className="title">Project Updates</h1>
-          <h2 className="subtitle">Dec 26, 2018</h2>
-          <article>
-            <p className="content">
-              The ImiRP website and application are currently undergoing
-              upgrades. As we make changes, important information about project
-              updates will be included here.
+
+          <div className="container" style={{ marginBottom: "50px" }}>
+            <h2 className="subtitle">Jan 8, 2019</h2>
+            <article>
+              <p className="content">
+                Added "ImiRP User Guide"
             </p>
-            <p>BR&TW</p>
-          </article>
+              <p>BR&TW</p>
+            </article>
+          </div>
+
+          <div className="container" style={{ marginBottom: "50px" }}>
+            <h2 className="subtitle">Dec 26, 2018</h2>
+            <article>
+              <p className="content">
+                The ImiRP website and application are currently undergoing
+                upgrades. As we make changes, important information about project
+                updates will be included here.
+            </p>
+              <p>BR&TW</p>
+            </article>
+          </div>
+
+
         </div>
       </section>
     </Layout>
